4125615f89dd845196f038aab32745547ce52d09
Jd0824um <jd0824um@minneapolis.edu>
2018-03-15 19:42:00 -0500

    Simple Factorial Calculator

---
+++ Factorial Calculator/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Factorial_Calculator
+{
+    partial class formFactorialCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtFactorial = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(37, 33);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(47, 13);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Factorial:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(100, 30);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(28, 20);
+            this.txtNumber.TabIndex = 1;
+            this.txtNumber.TextChanged += new System.EventHandler(this.clearFutureValue);
+            // 
+            // txtFactorial
+            // 
+            this.txtFactorial.Location = new System.Drawing.Point(100, 68);
+            this.txtFactorial.Name = "txtFactorial";
+            this.txtFactorial.ReadOnly = true;
+            this.txtFactorial.Size = new System.Drawing.Size(172, 20);
+            this.txtFactorial.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(100, 116);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(197, 116);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // formFactorialCalculator
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(284, 155);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtFactorial);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNumber);
+            this.Name = "formFactorialCalculator";
+            this.Text = "Factorial Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtFactorial;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Factorial Calculator/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial_Calculator
+{
+    public partial class formFactorialCalculator : Form
+    {
+        public formFactorialCalculator()
+        {
+            InitializeComponent();
+        }
+
+        //Closes program
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (isValidData())
+                {
+                    Int64 number = Int64.Parse(txtNumber.Text);
+                    Int64 result = number;
+
+                    //Gets the factorial result of the number using a for loop
+                    for (int x = 1; x < number; x++)
+                    {
+                        result = result * x;
+                    }
+
+                    //Sets txt box to the factorial result formatting commas
+                    txtFactorial.Text = result.ToString("N0");
+                }
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Please use smaller numbers.", "Entry Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.GetType().ToString(), ex.Message);
+            }
+        }
+
+        //Checks data to see if there is not a n empty string ##From lab
+        private bool isPresent(TextBox txtbox, string name)
+        {
+            if (txtbox.Text == "")
+            {
+                MessageBox.Show(name + " is a requred field.", "Entry Error");
+                txtbox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        //Checks the data to see if its a decimal number ##From lab
+        private bool isDigit(TextBox txtbox, string name)
+        {
+            Int64 number = 0;
+            if (Int64.TryParse(txtbox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a integer value.", "Entry Error");
+                txtbox.Focus();
+                return false;
+            }
+        }
+
+        //Checks the data to see if its greater than 1 and less than 20 ##From lab
+        private bool isWithinRange(TextBox txtbox, string name, int min, int max)
+        {
+            Int64 number = Convert.ToInt64(txtbox.Text);
+            if (number < min || number > max)
+            {
+                MessageBox.Show(name + " must be greater than " + min.ToString() + " and less than "
+                    + max.ToString() + ".", "Entry Error");
+                txtbox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+
+        //Checks input for valid data by calling methods to check for decimal, empty string, and in range
+        private bool isValidData()
+        {
+            return
+                isPresent(txtNumber, "Number") &&
+                isDigit(txtNumber, "Number") &&
+                isWithinRange(txtNumber, "Number", 1, 20);
+        }
+
+        //Clears values if number is changed
+        private void clearFutureValue(object sender, EventArgs e)
+        {
+            txtFactorial.Clear();
+        }
+
+    }
+}
